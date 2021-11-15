be1ff4a691e4fe6412dae855f94cc632bffe2cc5
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2021-11-15 17:55:26 -0300

    grid view in Reels Page

--- lib/pages/reels.dart
+++ lib/pages/reels.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
+import 'util/explore_grid.dart';
+
 class UserReels extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Reels')),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            color: Colors.grey[300],
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.grey[500],
+                ),
+                Container(
+                  child: Text(
+                    'Search',
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ExploreGrid(),
     );
   }
 }
